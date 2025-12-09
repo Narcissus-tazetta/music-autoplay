69badcc9ea911067de7edfdd78b53ba47672bfab
Narcissus-tazetta <akira_23s1101758@nnn.ed.jp>
2025-12-09 15:06:09 +0900

    fix: 🐛 aaaaaaa
    もう疲れた

--- src/app/components/ui/shadcn/table.tsx
+++ src/app/components/ui/shadcn/table.tsx
@@ -82,7 +82,7 @@ function TableHead<C extends React.ElementType = 'th'>({
         <Component
             data-slot='table-head'
             className={cn(
-                'text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 *:[[role=checkbox]]:translate-y-0.5',
+                'text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&_[role=checkbox]]:translate-y-0.5',
                 'className' in props && props.className,
             )}
             {...props}
@@ -101,7 +101,7 @@ function TableCell<C extends React.ElementType = 'td'>({
         <Component
             data-slot='table-cell'
             className={cn(
-                'p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 *:[[role=checkbox]]:translate-y-0.5',
+                'p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&_[role=checkbox]]:translate-y-0.5',
                 'className' in props && props.className,
             )}
             {...props}
